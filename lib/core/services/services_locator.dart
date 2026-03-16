25b57eb3005fb9142140a166f42b1b1dfa0e3899
Mina ubuntu <mina1335446@gmail.com>
2026-03-16 23:22:57 +0200

    presentation layer

--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -1,9 +1,11 @@
 import 'package:clean_architecture/movies/data/data_source/movie_remote_data_source.dart';
 import 'package:clean_architecture/movies/data/repository/movies_repository.dart';
 import 'package:clean_architecture/movies/domain/repository/base_movies_repository.dart';
+import 'package:clean_architecture/movies/domain/use_case/get_movie_detalis_usecase.dart';
 import 'package:clean_architecture/movies/domain/use_case/get_now_playing_movies_use_case.dart';
 import 'package:clean_architecture/movies/domain/use_case/get_popular_movies_use_case.dart';
 import 'package:clean_architecture/movies/domain/use_case/get_top_rated_movies_use_case.dart';
+import 'package:clean_architecture/movies/presentation/controller/movie_details_bloc.dart';
 import 'package:clean_architecture/movies/presentation/controller/movies_bloc.dart';
 import 'package:get_it/get_it.dart';
 
@@ -14,12 +16,16 @@ class ServiceLocator {
     /// Bloc
     sl.registerFactory(() => MoviesBloc(sl(), sl(), sl()));
 
+    sl.registerFactory(() => MovieDetailsBloc(sl()));
+
     ///USE CASE
     sl.registerLazySingleton(() => GetNowPlayingMoviesUseCase(sl()));
 
     sl.registerLazySingleton(() => GetPopularMoviesUseCase(sl()));
 
     sl.registerLazySingleton(() => GetTopRatedMoviesUseCase(sl()));
+
+    sl.registerLazySingleton(() => GetMovieDetailUseCase(sl()));
 
     /// REPOSITORIES
     sl.registerLazySingleton<BaseMoviesRepository>(
--- lib/movies/presentation/controller/movie_details_bloc.dart
+++ lib/movies/presentation/controller/movie_details_bloc.dart
@@ -1,16 +1,41 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:clean_architecture/core/utils/enums.dart';
 import 'package:clean_architecture/movies/domain/entities/movie_detail.dart';
+import 'package:clean_architecture/movies/domain/use_case/get_movie_detalis_usecase.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'movie_details_event.dart';
+
 part 'movie_details_state.dart';
 
 class MovieDetailsBloc extends Bloc<MovieDetailsEvent, MovieDetailsState> {
-  MovieDetailsBloc() : super(MovieDetailsState()) {
-    on<MovieDetailsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  MovieDetailsBloc(this.getMovieDetailUseCase) : super(MovieDetailsState()) {
+    on<GetMovieDetailsEvent>(_getMovieDetailsEvent);
+  }
+
+  final GetMovieDetailUseCase getMovieDetailUseCase;
+
+  FutureOr<void> _getMovieDetailsEvent(
+    GetMovieDetailsEvent event,
+    Emitter<MovieDetailsState> emit,
+  ) async {
+    final result = await getMovieDetailUseCase(
+      MovieDetailsParameters(movieId: event.id),
+    );
+
+    return result.fold(
+      (l) => emit(
+        state.copyWith(
+          movieDetailsState: RequestsState.error,
+          movieDetailsMessage: l.message,
+        ),
+      ),
+      (r) => emit(
+        state.copyWith(movieDetail: r, movieDetailsState: RequestsState.loaded),
+      ),
+    );
   }
 }
